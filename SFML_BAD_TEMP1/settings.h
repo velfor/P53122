393e09b5791759b13bfb761684cd30b947536e42
Paul Nikiforoff <velfor@tut.by>
2023-03-05 19:42:42 +0300

    Add files via upload

---
+++ SFML_BAD_TEMP1/settings.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+const float WINDOW_WIDTH = 1200;
+const float WINDOW_HEIGHT = 900;
+const std::string WINDOW_TITLE = "SFML Asteroids P53122";
+const float FPS = 60.f;
